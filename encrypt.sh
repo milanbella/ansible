721de35479b45299e7572eb48ba5a676c4e97241
Milan Bella <milanbella@hotmail.com>
2020-08-24 17:23:03 +0000

    m

--- encrypt.sh
+++ encrypt.sh
@@ -1,2 +1,4 @@
 set -x
 ansible-vault encrypt --vault-id develop@~/ansible_valut_password playbooks/test/roles/group_vars/postgres/vault/main.yml
+ansible-vault encrypt --vault-id develop@~/ansible_valut_password playbooks/idempiere/group_vars/idempiere/vault/main.yml
+ansible-vault encrypt --vault-id develop@~/ansible_valut_password playbooks/idempiere/group_vars/idempiere_db/vault/main.yml
