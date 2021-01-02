499db09106b3bee358377f36bd4c426ca4a015cd
Milan Bella <milanbella@hotmail.com>
2021-01-02 10:25:31 +0000

    role idempiere_standalone_server

--- decrypt.sh
+++ decrypt.sh
@@ -1,4 +1,3 @@
 set -x
-ansible-vault decrypt --vault-id develop@~/ansible_valut_password playbooks/test/roles/group_vars/postgres/vault/main.yml
-ansible-vault decrypt --vault-id develop@~/ansible_valut_password playbooks/idempiere/group_vars/idempiere/vault/main.yml
-ansible-vault decrypt --vault-id develop@~/ansible_valut_password playbooks/idempiere/group_vars/idempiere_db/vault/main.yml
+find . -path './playbooks/*/group_vars/*/vault/*' -exec ansible-vault decrypt --vault-id=develop@~/ansible_valut_password {} \;
+
